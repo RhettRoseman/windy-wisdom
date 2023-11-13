0d716d21804186f9839bc99c506cdf6ee85848de
RhettRoseman <rroseman@rcplanners.com>
2023-11-13 18:34:15 -0500

    finished product

--- js/script.js
+++ js/script.js
@@ -1,104 +1,48 @@
-// var repoList = document.getElementById('div2');
-// var fetchButton = document.getElementById('button');
-var APIkey = '54fcf8a6341230278157524bd285f72a' // API Key
-var button = document.getElementById('input');
-var button1 = document.getElementById('button');
-button.addEventListener('input', charlotteWeather);
-// button1.addEventListener('click', temperatures)
-// button.addEventListener('input', temps)
-// var cbutton = document.getElementById('charlotte');
-// var abutton = document.getElementById('atlanta');
-// var nbutton = document.getElementById('newyork');
-// var phbutton = document.getElementById('phoenix');
-// var aubutton = document.getElementById('austin');
-// var sbutton = document.getElementById('seattle');
-// var pobutton = document.getElementById('portland');
+document.addEventListener('DOMContentLoaded', function () {
+  const apiKey = '54fcf8a6341230278157524bd285f72a';
+  const apiUrlBase = 'https://api.openweathermap.org/data/2.5/forecast';
+  const form = document.querySelector('form');
+  const input = document.getElementById('input');
+  const cityName = document.getElementById('cityName');
 
-var charlotte = 'https://api.openweathermap.org/data/2.5/forecast?q=charlotte&appid=54fcf8a6341230278157524bd285f72a'; // WEATHER/ GEO API 
- 
-// var atlanta = 'https://api.openweathermap.org/data/2.5/forecast?q=atlanta&appid=54fcf8a6341230278157524bd285f72a';
+  function kelvinToFahrenheit(kelvin) {
+      return ((kelvin - 273.15) * 9/5 + 32).toFixed(2);
+  }
 
-// var newyork = 'https://api.openweathermap.org/data/2.5/forecast?q=newyork&appid=54fcf8a6341230278157524bd285f72a';
+  form.addEventListener('submit', function (event) {
+      event.preventDefault();
 
-// var phoenix = 'https://api.openweathermap.org/data/2.5/forecast?q=Phoenix&appid=54fcf8a6341230278157524bd285f72a';
+      const city = input.value;
 
-// var austin = 'https://api.openweathermap.org/data/2.5/forecast?q=austin&appid=54fcf8a6341230278157524bd285f72a';
+      if (city) {
+          const apiUrl = `${apiUrlBase}?q=${city}&appid=${apiKey}`;
 
-// var seattle = 'https://api.openweathermap.org/data/2.5/forecast?q=seattle&appid=54fcf8a6341230278157524bd285f72a'
+          fetch(apiUrl)
+              .then(response => response.json())
+              .then(data => {
+                  cityName.textContent = `Weather forecast for ${city}`;
 
-// var portland = 'https://api.openweathermap.org/data/2.5/forecast?q=portland&appid=54fcf8a6341230278157524bd285f72a'
+                  // Loop through the forecast data
+                  for (let i = 0; i < 5; i++) {
+                      const tableId = `day${i + 1}`;
+                      const dateId = `date${i + 1}`;
+                      const weatherId = `weather${i + 1}`;
+                      const temperatureId = `temperature${i + 1}`;
 
+                      const date = data.list[i].dt_txt;
+                      const weatherDescription = data.list[i].weather[0].description;
+                      const temperatureKelvin = data.list[i].main.temp;
+                      
+                      // Convert temperature from Kelvin to Fahrenheit
+                      const temperatureFahrenheit = kelvinToFahrenheit(temperatureKelvin);
 
-function charlotteWeather() {
-  fetch(charlotte)
-    .then(function (response) {
-      console.log(response);
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw response.json();
-      };
-      // then(data => {
-      //   console.log(data)
-
-      // })
-    })
-    .then(function (data) {
-      console.log(data);
-      for (var i = 0; i < data.length; i++) {
-        var listItem = document.getElementById('weather')
-        listItem.innerHTML="";
-        listItem.textContent = data[i].html_url;
-        repoList.appendChild(listItem);
+                      // Update the table cells with forecast information
+                      document.getElementById(dateId).textContent = date;
+                      document.getElementById(weatherId).textContent = weatherDescription;
+                      document.getElementById(temperatureId).textContent = `${temperatureFahrenheit}°F`;
+                  }
+              })
+              .catch(error => console.error(`Error fetching data: ${error}`));
       }
-      let kelvin = 293;
-const celsius = kelvin - 273;
-let fahrenheit = celsius * (9/5) + 32;
-console.log(fahrenheit);
-      document.getElementById("weather").textContent = data.list[0].weather[0].description;
-      console.log();
-      document.getElementById("temperature").textContent = data.list[0].main[0] = fahrenheit
-      console.log();
-      document.getElementById("cityName").textContent = data.city.name;
-      console.log();
-    });
-
-   
-    // let city = 'Charlotte';
-    // document.addEventListener('submit', () => {
-    //    city = document.getElementById('cityName').value;
-    //    askWeather.open('GET', 'url.../$(city)')
-    //    askWeather.send(); 
-    
-
-// function getApi() {
-//   fetch(atlanta)
-//     .then(function (response) {
-//       console.log(response);
-//       if (response.ok) {
-//         return response.json();
-//       } else {
-//         throw response.json();
-//       };
-//       // then(data => {
-//       //   console.log(data)
-
-//       // })
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//       for (var i = 0; i < data.length; i++) {
-//         var listItem = document.getElementById('cityName')
-//         listItem.innerHTML="";
-//         listItem.textContent = data[i].html_url;
-//         repoList.appendChild(listItem);
-//       }
-//       document.getElementById("cityName").textContent = data.list[0].weather[0].description;
-//     });
-
-
-
-// };
-// button.addEventListener('click', getApi);
- };
-       
+  });
+});
